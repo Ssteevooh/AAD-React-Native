a5c6f9781f48a995508d8ec4dcebb24429026d97
Steve Hommy <steve96hommy@gmail.com>
2022-11-13 20:35:33 +0200

    Signup

--- exercise04/pages/index.js
+++ exercise04/pages/index.js
@@ -10,6 +10,7 @@ import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
 import NotePage from './note';
+import SignUp from './signup';
 
 // Define routes
 const Pages = props => {
@@ -21,6 +22,7 @@ const Pages = props => {
                     <Route path="mynotes" element={<MyNotes/>} />
                     <Route path="favorites" element={<Favorites/>} />
                     <Route path='note/:id' element={<NotePage/>} />
+                    <Route path='signup' element={<SignUp/>} />
                 </Routes>
             </Layout>
         </Router>
