2ecfbc31d96a906ecb1250b252da4a998ec24fb7
Steve Hommy <steve96hommy@gmail.com>
2022-12-02 15:58:07 +0200

    Adding Screen Titles

--- exercise05/src/screens/index.js
+++ exercise05/src/screens/index.js
@@ -23,7 +23,7 @@ export default function Screens() {
     return (
         <Tab.Navigator initialRouteName='Feed'>
             <Tab.Screen name="Favorites" component={Favorites} options={{ title: 'Favorites' }} />
-            <Tab.Screen name="Feed" component={FeedStackScreen} options={{ title: 'Feed' }} />
+            <Tab.Screen name="Feed" component={FeedStackScreen} options={{ title: 'Feed', headerShown: false }} />
             <Tab.Screen name="My Notes" component={MyNotes} options={{ title: 'My Notes' }} />
         </Tab.Navigator>
     );
